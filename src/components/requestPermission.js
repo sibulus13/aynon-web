fb505b401b77b87206fc4c49fa47958c86040d01
Sib <chengjie.michael.huang@gmail.com>
2024-08-14 14:56:46 -0700

    removed redundant state

--- src/components/requestPermission.js
+++ src/components/requestPermission.js
@@ -1,21 +1,4 @@
-'use client'
-import { useEffect, useState } from 'react'
-
 export default function RequestPermission() {
-    const [location, setLocation] = useState();
-
-    useEffect(() => {
-        if ('geolocation' in navigator) {
-            // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
-            navigator.geolocation.getCurrentPosition(({ coords }) => {
-                const { latitude, longitude } = coords;
-                setLocation({ latitude, longitude });
-                console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-            })
-        }
-    }, []);
-
-
     return (
         <div>
             Please allow location access to continue.
