72d2ee09c7b23837fd6dd6bff67818f7324fc01c
Sib <chengjie.michael.huang@gmail.com>
2024-08-15 01:21:42 -0700

    indent

--- src/components/InitialLoad.js
+++ src/components/InitialLoad.js
@@ -3,7 +3,13 @@ export default function InitialLoad() {
     return (
         <div className="flex justify-center items-center h-screen">
             <div>
-                <ReactLoading type={'spin'} color={'black'} height={'30%'} width={'30%'} className='mx-auto m-2' />
+                <ReactLoading
+                    type={'spin'}
+                    color={'black'}
+                    height={'30%'}
+                    width={'30%'}
+                    className='mx-auto m-2'
+                />
                 <p className='text-center'>
                     Waiting for your whereabouts while we load the potato servers.
                 </p>
