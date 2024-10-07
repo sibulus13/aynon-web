dc2ff9593f2eb59b5861f6aebc931293b63128d3
Si8n <chengjie.michael.huang@gmail.com>
2024-10-07 16:33:32 -0700

    made home page public

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)', 
+  '/sign-up(.*)'
+])
 
 export default clerkMiddleware((auth, request) => {
+  console.log(request.url)
   if (!isPublicRoute(request)) {
     auth().protect()
   }
