d0d0d0e7bfe89805afc5ab9c36f0a942f3036c09
Sib <chengjie.michael.huang@gmail.com>
2024-10-09 12:57:22 -0700

    Updated metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AynonA",
+  title: "Aynon",
   description: "A Canadian anonymous social forum",
   rel: "icon",
   href: "/icon.ico",
