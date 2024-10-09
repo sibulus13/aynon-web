fc191764b20b21b19ba2419087896bd6996a17ec
Sib <chengjie.michael.huang@gmail.com>
2024-10-09 11:40:58 -0700

    Updated description

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ const Page = () => {
       className='flex flex-col items-center justify-center h-screen gap-4'
     >
       <h1>Find your gaggle.</h1>
-      <h2>Welcome to Aynon, A Canadian anonymous social forum</h2>
+      <h2>Welcome to Aynon, A minimalist Canadian anonymous social forum</h2>
       <Link href='/App' className='border-2 rounded-3xl p-2 hover:border-black'>
         Click here for the app
       </Link>
